ca7a9f7af3915ee25262023f6efde083ec39e9af
Zintom <tzsinkoapps@gmail.com>
2020-10-25 19:59:26 +0000

    Create BitMask.cs
    Added BitMask.cs helper extension class for Setting and Removing flags from flag enums.

---
+++ GameOptimizer/GameOptimizer/Helpers/BitMask.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOptimizer
+{
+    /// <summary>
+    /// Helper class for using Flags/Bitmasks on <see cref="Enum"/> types.
+    /// </summary>
+    public static class BitMask
+    {
+
+        /// <summary>
+        /// Adds <paramref name="flag"/> to <paramref name="flags"/>.
+        /// <para/>
+        /// Equivalent to '<c>flags OR flag</c>'.
+        /// </summary>
+        public static void SetFlag<TEnum>(ref this TEnum flags, TEnum flag) where TEnum : struct, Enum
+        {
+            int flagsValue = (int)(object)flags;
+            int flagValue = (int)(object)flag;
+
+            flags = (TEnum)(object)(flagsValue | flagValue);
+        }
+
+        /// <summary>
+        /// Removes <paramref name="flag"/> from <paramref name="flags"/>.
+        /// <para/>
+        /// Equivalent to '<c>flags AND NOT flag</c>'.
+        /// </summary>
+        public static void RemoveFlag<TEnum>(ref this TEnum flags, TEnum flag) where TEnum : struct, Enum
+        {
+            int flagsValue = (int)(object)flags;
+            int flagValue = (int)(object)flag;
+
+            flags = (TEnum)(object)(flagsValue & ~flagValue);
+        }
+
+    }
+}

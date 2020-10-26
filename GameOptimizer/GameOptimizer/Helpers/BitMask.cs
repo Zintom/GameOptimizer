ca5a8e24a723a8c0f12070af1b7b6862d8eb2bfd
Zintom <tzsinkoapps@gmail.com>
2020-10-26 15:13:39 +0000

    Added Set/UnsetBit and Set/UnsetBitRange methods
    Added four new methods, SetBit(), UnsetBit(), SetBitRange() and UnsetBitRange().
    
    These are helper methods to assist with creating dynamic bitmasks/fields, specifically used in the Optimizer for setting processor affinities which use an IntPtr bitfield.

--- GameOptimizer/GameOptimizer/Helpers/BitMask.cs
+++ GameOptimizer/GameOptimizer/Helpers/BitMask.cs
@@ -38,5 +38,63 @@ namespace GameOptimizer
             flags = (TEnum)(object)(flagsValue & ~flagValue);
         }
 
+        /// <summary>
+        /// Sets the bit at the given index.
+        /// </summary>
+        /// <param name="bitIndex">The index of the bit you wish to set <b>(evaluated right to left)</b>.</param>
+        public static int SetBit(this int val, int bitIndex)
+        {
+            int mask = 1 << bitIndex;
+            return val |= mask;
+        }
+
+        /// <summary>
+        /// Unsets the bit at the given index.
+        /// </summary>
+        /// <param name="bitIndex">The index of the bit you wish to unset <b>(evaluated right to left)</b>.</param>
+        public static int UnsetBit(this int val, int bitIndex)
+        {
+            int mask = 1 << bitIndex;
+            return val &= ~mask;
+        }
+
+        /// <summary>
+        /// Sets the range of bits between the <paramref name="startIndex"/> and <paramref name="endIndex"/>.
+        /// </summary>
+        /// <param name="val">The value to modify.</param>
+        /// <param name="startIndex">The index of the start bit <b>(evaluated right to left)</b>.</param>
+        /// <param name="endIndex">The index of the end bit <b>(evaluated right to left)</b>.</param>
+        public static int SetBitRange(this int val, int startIndex, int endIndex)
+        {
+            if (startIndex < 0) throw new IndexOutOfRangeException("The start index must be positive.");
+            if (endIndex > 31) throw new IndexOutOfRangeException("The end index must be less than 32(the number of bits in an Int32).");
+
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                val = val.SetBit(i);
+            }
+
+            return val;
+        }
+
+        /// <summary>
+        /// Unsets the range of bits between the <paramref name="startIndex"/> and <paramref name="endIndex"/>.
+        /// </summary>
+        /// <param name="val">The value to modify.</param>
+        /// <param name="startIndex">The index of the start bit <b>(evaluated right to left)</b>.</param>
+        /// <param name="endIndex">The index of the end bit <b>(evaluated right to left)</b>.</param>
+        public static int UnsetBitRange(this int val, int startIndex, int endIndex)
+        {
+            if (startIndex < 0) throw new IndexOutOfRangeException("The start index must be positive.");
+            if (endIndex > 31) throw new IndexOutOfRangeException("The end index must be less than 32(the number of bits in an Int32).");
+
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                val = val.UnsetBit(i);
+            }
+
+            return val;
+        }
+
     }
 }

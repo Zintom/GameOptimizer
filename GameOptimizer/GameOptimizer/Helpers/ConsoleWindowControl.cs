1fb84e5de7cf761e43a20806ce7367de1be60e0f
Zintom <tzsinkoapps@gmail.com>
2020-10-26 09:54:01 +0000

    Decoupled the P/Invoke from Program.cs
    Decoupled the P/Invoke to ShowWindow() and GetConsoleWindow() from Program.cs.
    
    Now it exists in a helper class ConsoleWindowControl, its functionality can be used via the MinimizeConsoleWindow helper method.

---
+++ GameOptimizer/GameOptimizer/Helpers/ConsoleWindowControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOptimizer.Helpers
+{
+    /// <summary>
+    /// Helper class for controlling the console window.
+    /// </summary>
+    public static class ConsoleWindowControl
+    {
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private const int SW_MINIMIZE = 6;
+
+        /// <summary>
+        /// Minimizes the console window.
+        /// </summary>
+        public static void MinimizeConsoleWindow()
+        {
+            ShowWindow(GetConsoleWindow(), SW_MINIMIZE);
+        }
+    }
+}

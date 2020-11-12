8f36fce6216a54bf77af051a25f80ee0faa2ee12
Zintom <tzsinkoapps@gmail.com>
2020-11-12 21:29:32 +0000

    Bitmask now uses ulong so that it may work with large enums

--- GameOptimizer/GameOptimizer/Helpers/BitMask.cs
+++ GameOptimizer/GameOptimizer/Helpers/BitMask.cs
@@ -42,9 +42,9 @@ namespace Zintom.GameOptimizer
         /// Sets the bit at the given index.
         /// </summary>
         /// <param name="bitIndex">The index of the bit you wish to set <b>(evaluated right to left)</b>.</param>
-        public static int SetBit(this int val, int bitIndex)
+        public static ulong SetBit(this ulong val, int bitIndex)
         {
-            int mask = 1 << bitIndex;
+            ulong mask = 1ul << bitIndex;
             return val |= mask;
         }
 
@@ -52,9 +52,9 @@ namespace Zintom.GameOptimizer
         /// Unsets the bit at the given index.
         /// </summary>
         /// <param name="bitIndex">The index of the bit you wish to unset <b>(evaluated right to left)</b>.</param>
-        public static int UnsetBit(this int val, int bitIndex)
+        public static ulong UnsetBit(this ulong val, int bitIndex)
         {
-            int mask = 1 << bitIndex;
+            ulong mask = 1ul << bitIndex;
             return val &= ~mask;
         }
 
@@ -64,17 +64,9 @@ namespace Zintom.GameOptimizer
         /// <param name="val">The value to modify.</param>
         /// <param name="startIndex">The index of the start bit <b>(evaluated right to left)</b>.</param>
         /// <param name="endIndex">The index of the end bit <b>(evaluated right to left)</b>.</param>
-        public static int SetBitRange(this int val, int startIndex, int endIndex)
+        public static ulong SetBitRange(this ulong val, int startIndex, int endIndex)
         {
-            if (startIndex < 0) throw new ArgumentOutOfRangeException($"The {nameof(startIndex)} must be positive.");
-            if (endIndex > 31) throw new ArgumentOutOfRangeException($"The {nameof(endIndex)} must be less than 32(the number of bits in an Int32).");
-
-            for (int i = startIndex; i < endIndex; i++)
-            {
-                val = val.SetBit(i);
-            }
-
-            return val;
+            return ModifyBitRange(val, startIndex, endIndex, true);
         }
 
         /// <summary>
@@ -83,14 +75,26 @@ namespace Zintom.GameOptimizer
         /// <param name="val">The value to modify.</param>
         /// <param name="startIndex">The index of the start bit <b>(evaluated right to left)</b>.</param>
         /// <param name="endIndex">The index of the end bit <b>(evaluated right to left)</b>.</param>
-        public static int UnsetBitRange(this int val, int startIndex, int endIndex)
+        public static ulong UnsetBitRange(this ulong val, int startIndex, int endIndex)
+        {
+            return ModifyBitRange(val, startIndex, endIndex, false);
+        }
+
+        /// <param name="val">The value to modify.</param>
+        /// <param name="startIndex">The index of the start bit <b>(evaluated right to left)</b>.</param>
+        /// <param name="endIndex">The index of the end bit <b>(evaluated right to left)</b>.</param>
+        /// <param name="setBits">Default behaviour is to unset the bits, set to <see langword="true"/> to set bits.</param>
+        private static ulong ModifyBitRange(this ulong val, int startIndex, int endIndex, bool setBits)
         {
             if (startIndex < 0) throw new ArgumentOutOfRangeException($"The {nameof(startIndex)} must be positive.");
-            if (endIndex > 31) throw new ArgumentOutOfRangeException($"The {nameof(endIndex)} must be less than 32(the number of bits in an Int32).");
+            if (endIndex > 64) throw new ArgumentOutOfRangeException($"The {nameof(endIndex)} must be less than or equal to 64(the number of bits in a UInt64).");
 
             for (int i = startIndex; i < endIndex; i++)
             {
-                val = val.UnsetBit(i);
+                if (setBits)
+                    val = SetBit(val, i);
+                else
+                    val = UnsetBit(val, i);
             }
 
             return val;

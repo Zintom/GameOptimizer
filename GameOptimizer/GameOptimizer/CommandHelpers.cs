a65f17b3df6bda8c281a298f843b1e30b3084096
Zintom <tzsinkoapps@gmail.com>
2020-11-24 16:29:40 +0000

    Update CommandHelpers.cs
    Now uses InteractiveShell rather than Console.WriteLine.
    
    Removed Command_OptimizeNoFlags() as this was uncessary, callers should use Command_OptimizeWithFlags(None).
    
    The number of restores/optimizations are displayed at the end of processing.

--- GameOptimizer/GameOptimizer/CommandHelpers.cs
+++ GameOptimizer/GameOptimizer/CommandHelpers.cs
@@ -9,30 +9,14 @@ namespace Zintom.GameOptimizer
         private const string Text_PressAnyKeyToGoBack = "Press any key to go back.";
         private const string Text_Optimized = "\nOptimized.";
 
-        static void Command_OptimizeNoFlags()
-        {
-            _interactiveShell.DrawTitle(AppName, $"Optimizing in {_optimizeWaitTimeMillis / 1000} seconds...", null, true);
-            Thread.Sleep(_optimizeWaitTimeMillis);
-            optimizer.Optimize();
-
-            _interactiveShell.DrawSubtitleText(Text_Optimized);
-
-            Thread.Sleep(1500);
-            NativeMethods.MinimizeConsoleWindow();
-
-            _interactiveShell.DrawContentText(Text_PressAnyKeyToGoBack, false); Console.ReadKey();
-        }
-
         static void Command_OptimizeWithFlags(OptimizeConditions flags)
         {
-            _interactiveShell.DrawTitle(AppName, $"Optimizing in {_optimizeWaitTimeMillis / 1000} seconds...", null, true);
+            _gui.DrawTitle(AppName, $"Optimizing in {_optimizeWaitTimeMillis / 1000} seconds...", null, true);
             Thread.Sleep(_optimizeWaitTimeMillis);
 
-            _interactiveShell.DrawContentText($"Optimizing (Flags: {flags})...");
+            _gui.DrawContentText($"Optimizing (Flags: {flags})...");
 
-            optimizer.Optimize(flags);
-
-            _interactiveShell.DrawSubtitleText(Text_Optimized);
+            _gui.DrawSubtitleText(string.Format("\nCompleted {0} optimizations.", optimizer.Optimize(flags)));
 
             if (!flags.HasFlag(OptimizeConditions.NoHide))
             {
@@ -40,29 +24,25 @@ namespace Zintom.GameOptimizer
                 NativeMethods.MinimizeConsoleWindow();
             }
 
-            _interactiveShell.DrawContentText(Text_PressAnyKeyToGoBack, false); Console.ReadKey();
+            _gui.DrawContentText(Text_PressAnyKeyToGoBack, false); Console.ReadKey();
         }
 
         static void Command_Restore()
         {
-            _interactiveShell.DrawTitle(AppName, "Restoring", null, true);
+            _gui.DrawTitle(AppName, "Restoring", null, true);
 
-            optimizer.Restore();
+            _gui.DrawSubtitleText(string.Format("\nCompleted {0} restore operations.", optimizer.Restore()));
 
-            _interactiveShell.DrawSubtitleText("\nRestored.");
-
-            _interactiveShell.DrawContentText(Text_PressAnyKeyToGoBack, false); Console.ReadKey();
+            _gui.DrawContentText(Text_PressAnyKeyToGoBack, false); Console.ReadKey();
         }
 
         static void Command_ForceRestore()
         {
-            _interactiveShell.DrawTitle(AppName, "Force restoring process priorities to Normal.", null, true);
+            _gui.DrawTitle(AppName, "Force restoring process priorities to Normal.", null, true);
 
-            optimizer.ForceRestoreToNormal();
+            _gui.DrawSubtitleText(string.Format("\n{0} processes affected by the force-restore.", optimizer.ForceRestoreToNormal()));
 
-            _interactiveShell.DrawSubtitleText("\nRestored.");
-
-            _interactiveShell.DrawContentText(Text_PressAnyKeyToGoBack, false); Console.ReadKey();
+            _gui.DrawContentText(Text_PressAnyKeyToGoBack, false); Console.ReadKey();
         }
 
     }

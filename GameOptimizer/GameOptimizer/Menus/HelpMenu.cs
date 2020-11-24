af2b57eae0d9b57b5b59c33406aa1ad1a842e1dc
Zintom <tzsinkoapps@gmail.com>
2020-11-24 21:09:46 +0000

    Independent Help menu.

---
+++ GameOptimizer/GameOptimizer/Menus/HelpMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace Zintom.GameOptimizer.Menus
+{
+    public class HelpMenu : IConsoleMenu
+    {
+        public void Run(InteractiveShell.InteractiveShell gui)
+        {
+        reset:
+            gui.DrawTitle(Program.AppName,
+            "Help", "opt         | Optimizes games by isolating cores and adjusting low priorities." +
+            "\nres         | Restores all processes back to normal." +
+            "\nres -force  | Forces all processes to Normal Priority and Affinity 'All Cores'" +
+            "\nedit        | Allows you to edit the priorty process list." +
+            "\naudio       | Launches SndVol.exe -f allowing you to change the computers master volume." +
+            "\naudio mixer | Launches SndVol.exe -m opening the volume mixer."
+            , null, true);
+            int result = gui.DisplayMenu(new string[] { "Open README.txt", "Open LICENSE.txt", "Back" });
+
+            if (result == 0) { Process.Start("notepad.exe", "README.txt"); goto reset; }
+            if (result == 1) { Process.Start("notepad.exe", "LICENSE.txt"); goto reset; }
+        }
+    }
+}

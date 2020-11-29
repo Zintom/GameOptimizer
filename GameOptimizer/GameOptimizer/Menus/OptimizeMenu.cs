bd97837d37313d4139e2c7a19e26d18da850990b
Zintom <tzsinkoapps@gmail.com>
2020-11-29 12:57:53 +0000

    Removed expert-level optimization methods.
    Removed expert-level optimization methods from the 'Optimize' screen as these have consequences that only expert-level users will be able to comprehend. For instance option 4 can introduce serious network lag as games etc will be prioritised over SVCHost which manages networking.

--- GameOptimizer/GameOptimizer/Menus/OptimizeMenu.cs
+++ GameOptimizer/GameOptimizer/Menus/OptimizeMenu.cs
@@ -7,14 +7,20 @@ namespace Zintom.GameOptimizer.Menus
 
         public void Run(InteractiveShell.InteractiveShell gui)
         {
-            gui.DrawTitle(Program.AppName, "Select a quick option to execute", null, true);
+            gui.DrawTitle(Program.AppName, "Select an optimization method to execute:", null, true);
             string[] quickCommands = new string[] { "1: Default optimization",
                             "2: Default optimization plus Affinity optimization",
-                            "3: Boost whitelisted processes and de-prioritise everything else.",
-                            "4: Just boost whitelisted processes without touching other processes",
                             "Back"};
 
-            int result = gui.DisplayMenu(quickCommands);
+            string[] footerTexts = new string[] {
+                "Sets all non-whitelisted processes to 'Low' priority,   \nwhilst leaving whitelisted processes at 'Normal' priority.              \n                       ",
+                "Manages process priorties in the same way as option 1   \nwhilst also restricting all non-whitelisted processes to use only       \nthe last two CPU cores.",
+                "Go back to the previous menu.                           \n                                                                        \n                       "
+            };
+
+            gui.FallbackDisplayOptions.FooterVerticalPadding = 2;
+            gui.FallbackDisplayOptions.FooterForegroundColour = ConsoleColor.Cyan;
+            int result = gui.DisplayMenu(quickCommands, footerTexts);
 
             switch (result)
             {
@@ -24,12 +30,12 @@ namespace Zintom.GameOptimizer.Menus
                 case 1:
                     Program.Command_OptimizeWithFlags(OptimizeConditions.OptimizeAffinity);
                     break;
-                case 2:
-                    Program.Command_OptimizeWithFlags(OptimizeConditions.BoostPriorities);
-                    break;
-                case 3:
-                    Program.Command_OptimizeWithFlags(OptimizeConditions.BoostPriorities | OptimizeConditions.IgnoreOrdinaryProcesses);
-                    break;
+                //case 2:
+                //    Program.Command_OptimizeWithFlags(OptimizeConditions.BoostPriorities);
+                //    break;
+                //case 3:
+                //    Program.Command_OptimizeWithFlags(OptimizeConditions.BoostPriorities | OptimizeConditions.IgnoreOrdinaryProcesses);
+                //    break;
                 default:
                     return;
             }

b4c0bdc5ccd5359fc6525ff847b798b6d40531bd
Zintom <tzsinkoapps@gmail.com>
2020-11-29 11:38:32 +0000

    Removed toxic `goto` statement.
    Opted for a while loop rather than a nasty `goto`.

--- GameOptimizer/GameOptimizer/Menus/HelpMenu.cs
+++ GameOptimizer/GameOptimizer/Menus/HelpMenu.cs
@@ -6,19 +6,31 @@ namespace Zintom.GameOptimizer.Menus
     {
         public void Run(InteractiveShell.InteractiveShell gui)
         {
-        reset:
-            gui.DrawTitle(Program.AppName,
-            "Help", "opt         | Optimizes games by isolating cores and adjusting low priorities." +
-            "\nres         | Restores all processes back to normal." +
-            "\nres -force  | Forces all processes to Normal Priority and Affinity 'All Cores'" +
-            "\nedit        | Allows you to edit the priorty process list." +
-            "\naudio       | Launches SndVol.exe -f allowing you to change the computers master volume." +
-            "\naudio mixer | Launches SndVol.exe -m opening the volume mixer."
-            , null, true);
-            int result = gui.DisplayMenu(new string[] { "Open README.txt", "Open LICENSE.txt", "Back" });
+            int selectedOption = 0;
 
-            if (result == 0) { Process.Start("notepad.exe", "README.txt"); goto reset; }
-            if (result == 1) { Process.Start("notepad.exe", "LICENSE.txt"); goto reset; }
+            while (true)
+            {
+                gui.DrawTitle(Program.AppName,
+                "Help", "opt         | Optimizes games by isolating cores and adjusting low priorities." +
+                "\nres         | Restores all processes back to normal." +
+                "\nres -force  | Forces all processes to Normal Priority and Affinity 'All Cores'" +
+                "\nedit        | Allows you to edit the priorty process list." +
+                "\naudio       | Launches SndVol.exe -f allowing you to change the computers master volume." +
+                "\naudio mixer | Launches SndVol.exe -m opening the volume mixer."
+                , null, true);
+
+                selectedOption = gui.DisplayMenu(new string[] { "Open README.txt", "Open LICENSE.txt", "Back" }, null, selectedOption);
+
+                switch (selectedOption)
+                {
+                    case 0:
+                        Process.Start("notepad.exe", "README.txt"); break;
+                    case 1:
+                        Process.Start("notepad.exe", "LICENSE.txt"); break;
+                    default:
+                        return;
+                }
+            }
         }
     }
 }

646ecc583a51dadfc3c4c4a799e970352a7ebdc6
Zintom <tzsinkoapps@gmail.com>
2022-01-07 11:25:55 +0000

    Moved the whitelist reader into its own class.

--- GameOptimizer/GameOptimizer/Program.cs
+++ GameOptimizer/GameOptimizer/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Reflection;
 using Zintom.GameOptimizer.Menus;
 using Zintom.InteractiveShell;
 
@@ -13,9 +10,7 @@ namespace Zintom.GameOptimizer
     {
 
         public static string AppName = "Zintom's Game Optimizer";
-        public const string WhitelistFile = "process_whitelist.txt";
         internal const string ConfigFile = "config.json";
-        const string FileComment = "#";
 
         const int _optimizeWaitTimeMillis = 5000;
 
@@ -43,7 +38,7 @@ namespace Zintom.GameOptimizer
             SetupInteractiveShell();
             ConfigManager.WriteDefaultConfigIfNotExists(ConfigFile);
 
-            Optimizer = new Optimizer(GetWhitelistedProcessNames(outputDisplayer), ConfigManager.Read(ConfigFile), outputDisplayer);
+            Optimizer = new Optimizer(WhitelistReader.GetWhitelistedProcessNames(outputDisplayer), ConfigManager.Read(ConfigFile), outputDisplayer);
 
             // Begin Main Menu application loop
             IConsoleMenu mainMenu = new MainMenu();
@@ -107,67 +102,6 @@ namespace Zintom.GameOptimizer
         {
             Console.BackgroundColor = _defaultBackColor;
             Console.ForegroundColor = _defaultForeColor;
-        }
-
-        /// <summary>
-        /// Creates the default priority proccesses file if it does not exist.
-        /// </summary>
-        static void CreateDefaultProcessWhitelistFile()
-        {
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            string defaultListResource = "Zintom.GameOptimizer.Assets.default_process_whitelist.txt";
-
-            // Try get the text resource.
-            using (var resourceStream = assembly.GetManifestResourceStream(defaultListResource))
-            {
-                if (resourceStream == null)
-                {
-                    Console.WriteLine(string.Format("Error occurred when loading default whitelist file, '{0}' not found in resources or could not be loaded.", WhitelistFile));
-                    return;
-                }
-
-                using (var resourceReader = new StreamReader(resourceStream))
-                using (var fileStream = File.Create(WhitelistFile))
-                using (var fileWriter = new StreamWriter(fileStream))
-                {
-                    // Write the resource to file, flush and close.
-                    fileWriter.Write(resourceReader.ReadToEnd());
-                    fileWriter.Flush();
-                    fileWriter.Close();
-
-                    Console.WriteLine($"Generated '{WhitelistFile}'");
-                }
-            }
-        }
-
-        /// <summary>
-        /// Reads the <see cref="WhitelistFile"/> and returns the priority process names from within.
-        /// </summary>
-        /// <returns>All the priority process names in a <see cref="string"/> array.</returns>
-        static List<string> GetWhitelistedProcessNames(IOutputProvider outputDisplayer)
-        {
-            if (!File.Exists(WhitelistFile))
-            {
-                CreateDefaultProcessWhitelistFile();
-            }
-
-            outputDisplayer.Output(string.Format("Reading whitelist file '{0}'..", WhitelistFile));
-
-            List<string> whitelistedProcessNames = new List<string>();
-
-            string[] lines = File.ReadAllLines(WhitelistFile);
-
-            for (int i = 0; i < lines.Length; i++)
-            {
-                if (lines[i].Contains(FileComment, StringComparison.InvariantCulture))
-                    whitelistedProcessNames.Add(lines[i].Substring(0, lines[i].IndexOf(FileComment)).Trim());
-                else
-                    whitelistedProcessNames.Add(lines[i].Trim());
-            }
-
-            outputDisplayer.Output(string.Format("'{0}' loaded.", WhitelistFile));
-
-            return whitelistedProcessNames;
         }
 
         /// <summary>
---
+++ GameOptimizer/GameOptimizer/WhitelistReader.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace Zintom.GameOptimizer
+{
+    internal class WhitelistReader
+    {
+
+        internal const string WhitelistFile = "process_whitelist.txt";
+        private const string FileComment = "#";
+
+        /// <summary>
+        /// Creates the default priority proccesses file if it does not exist.
+        /// </summary>
+        private static void CreateDefaultProcessWhitelistFile()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string defaultListResource = "Zintom.GameOptimizer.Assets.default_process_whitelist.txt";
+
+            // Try get the text resource.
+            using (var resourceStream = assembly.GetManifestResourceStream(defaultListResource))
+            {
+                if (resourceStream == null)
+                {
+                    Console.WriteLine(string.Format("Error occurred when loading default whitelist file, '{0}' not found in resources or could not be loaded.", WhitelistFile));
+                    return;
+                }
+
+                using (var resourceReader = new StreamReader(resourceStream))
+                using (var fileStream = File.Create(WhitelistFile))
+                using (var fileWriter = new StreamWriter(fileStream))
+                {
+                    // Write the resource to file, flush and close.
+                    fileWriter.Write(resourceReader.ReadToEnd());
+                    fileWriter.Flush();
+                    fileWriter.Close();
+
+                    Console.WriteLine($"Generated '{WhitelistFile}'");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Reads the <see cref="WhitelistFile"/> and returns the priority process names from within.
+        /// </summary>
+        /// <returns>All the priority process names in a <see cref="string"/> array.</returns>
+        internal static List<string> GetWhitelistedProcessNames(IOutputProvider outputDisplayer)
+        {
+            if (!File.Exists(WhitelistFile))
+            {
+                CreateDefaultProcessWhitelistFile();
+            }
+
+            outputDisplayer.Output(string.Format("Reading whitelist file '{0}'..", WhitelistFile));
+
+            List<string> whitelistedProcessNames = new List<string>();
+
+            string[] lines = File.ReadAllLines(WhitelistFile);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i].Contains(FileComment, StringComparison.InvariantCulture))
+                    whitelistedProcessNames.Add(lines[i].Substring(0, lines[i].IndexOf(FileComment)).Trim());
+                else
+                    whitelistedProcessNames.Add(lines[i].Trim());
+            }
+
+            outputDisplayer.Output(string.Format("'{0}' loaded.", WhitelistFile));
+
+            return whitelistedProcessNames;
+        }
+
+    }
+}

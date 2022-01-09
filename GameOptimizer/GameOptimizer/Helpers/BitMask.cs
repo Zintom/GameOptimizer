6eafdced0df162e4d8d8ff49520a887d8a4d68a0
Zintom <tzsinkoapps@gmail.com>
2022-01-09 17:38:55 +0000

    Improved code docs and exposed the ModifyBitRange method.

--- GameOptimizer/GameOptimizer/Helpers/BitMask.cs
+++ GameOptimizer/GameOptimizer/Helpers/BitMask.cs
@@ -6,7 +6,7 @@ namespace Zintom.GameOptimizer
     /// Helper class for using Flags/Bitmasks on <see cref="Enum"/> types.
     /// </summary>
     /// <remarks><b>Note: </b>All bit related operations start at the least significant bit.</remarks>
-    public static class BitMask
+    internal static class BitMask
     {
 
         /// <summary>
@@ -14,7 +14,7 @@ namespace Zintom.GameOptimizer
         /// <para/>
         /// Equivalent to '<c>flags OR flag</c>'.
         /// </summary>
-        public static void SetFlag<TEnum>(ref this TEnum flags, TEnum flag) where TEnum : struct, Enum
+        internal static void SetFlag<TEnum>(ref this TEnum flags, TEnum flag) where TEnum : struct, Enum
         {
             int flagsValue = (int)(object)flags;
             int flagValue = (int)(object)flag;
@@ -27,7 +27,7 @@ namespace Zintom.GameOptimizer
         /// <para/>
         /// Equivalent to '<c>flags AND NOT flag</c>'.
         /// </summary>
-        public static void RemoveFlag<TEnum>(ref this TEnum flags, TEnum flag) where TEnum : struct, Enum
+        internal static void RemoveFlag<TEnum>(ref this TEnum flags, TEnum flag) where TEnum : struct, Enum
         {
             int flagsValue = (int)(object)flags;
             int flagValue = (int)(object)flag;
@@ -39,7 +39,7 @@ namespace Zintom.GameOptimizer
         /// Sets the bit at the given index.
         /// </summary>
         /// <param name="bitIndex">The index of the bit you wish to set.</param>
-        public static nint SetBit(nint val, int bitIndex)
+        internal static nint SetBit(nint val, int bitIndex)
         {
             nint mask = (nint)1 << bitIndex;
             return val |= mask;
@@ -49,39 +49,39 @@ namespace Zintom.GameOptimizer
         /// Unsets the bit at the given index.
         /// </summary>
         /// <param name="bitIndex">The index of the bit you wish to unset.</param>
-        public static nint UnsetBit(nint val, int bitIndex)
+        internal static nint UnsetBit(nint val, int bitIndex)
         {
             nint mask = (nint)1 << bitIndex;
             return val &= ~mask;
         }
 
         /// <summary>
-        /// Sets the range of bits between the <paramref name="startIndex"/> and <paramref name="endIndex"/>.
+        /// Sets the range of bits from the <paramref name="startIndex"/> for <paramref name="count"/> binary digits.
         /// </summary>
         /// <param name="val">The value to modify.</param>
         /// <param name="startIndex">The index of the start bit.</param>
-        /// <param name="endIndex">The index of the end bit.</param>
-        public static nint SetBitRange(nint val, int startIndex, int count)
+        internal static nint SetBitRange(nint val, int startIndex, int count)
         {
             return ModifyBitRange(val, startIndex, count, true);
         }
 
         /// <summary>
-        /// Unsets the range of bits between the <paramref name="startIndex"/> and <paramref name="endIndex"/>.
+        /// Unsets the range of bits from the <paramref name="startIndex"/> for <paramref name="count"/> binary digits.
         /// </summary>
         /// <param name="val">The value to modify.</param>
-        /// <param name="startIndex">The index of the start bit <b>(evaluated right to left)</b>.</param>
-        /// <param name="endIndex">The index of the end bit <b>(evaluated right to left)</b>.</param>
-        public static nint UnsetBitRange(nint val, int startIndex, int count)
+        /// <param name="startIndex">The index of the start bit.</param>
+        internal static nint UnsetBitRange(nint val, int startIndex, int count)
         {
             return ModifyBitRange(val, startIndex, count, false);
         }
 
+        /// <summary>
+        /// Sets or Unsets the range of bits from the <paramref name="startIndex"/> for <paramref name="count"/> binary digits.
+        /// </summary>
         /// <param name="val">The value to modify.</param>
-        /// <param name="startIndex">The index of the start bit <b>(evaluated right to left)</b>.</param>
-        /// <param name="endIndex">The index of the end bit <b>(evaluated right to left)</b>.</param>
-        /// <param name="setBits">Default behaviour is to unset the bits, set to <see langword="true"/> to set bits.</param>
-        private static nint ModifyBitRange(nint val, int startIndex, int count, bool setBits)
+        /// <param name="startIndex">The index of the start bit.</param>
+        /// <param name="setBits">Use <see langword="true"/> to set the range of bits, or <see langword="false"/> to unset them.</param>
+        internal static nint ModifyBitRange(nint val, int startIndex, int count, bool setBits)
         {
             if (startIndex < 0) throw new ArgumentOutOfRangeException($"The {nameof(startIndex)} must be positive.");
 

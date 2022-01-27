be16e5997393afc082ec7dd06fd20859877dc232
Zintom <tzsinkoapps@gmail.com>
2022-01-27 12:49:22 +0000

    Create ActiveProcessProvider.cs
    Provides a way of retrieving all the active processes on the system without going through `Process.GetProcesses()`, this way we can mock an `IActiveProcessProvider` in our tests.

---
+++ GameOptimizer/GameOptimizer/Optimization/ActiveProcessProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Zintom.GameOptimizer.Optimization
+{
+    internal interface IActiveProcessProvider
+    {
+        /// <inheritdoc cref="System.Diagnostics.Process.GetProcesses"/>
+        IProcess[] GetProcesses();
+    }
+
+    /// <summary>
+    /// Provides an array of all active processes on this system.
+    /// </summary>
+    internal class ActiveProcessProvider : IActiveProcessProvider
+    {
+        /// <summary>
+        /// Abstracts <see cref="System.Diagnostics.Process.GetProcesses"/>
+        /// </summary>
+        public IProcess[] GetProcesses() => Array.ConvertAll(System.Diagnostics.Process.GetProcesses(), p => new ProcessWrapper(p));
+    }
+}
